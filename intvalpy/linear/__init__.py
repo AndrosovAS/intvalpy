a6c2f87e5a81d46d10c0e401a52f24bc58ab8cf4
Maestross <61318188+Maestross@users.noreply.github.com>
2021-03-01 23:29:15 +0700

    bugs fixed

--- intvalpy/linear/__init__.py
+++ intvalpy/linear/__init__.py
@@ -1,4 +1,4 @@
 from .overdetermined import Rohn, PSS
 from .square_system import Gauss, Gauss_Seidel
 from .system_properties import Uni, Tol, ive
-from .solution_set import lineqs, IntLinIncR2
+from .solution_set import lineqs, IntLinIncR2, lineqs3D, IntLinIncR3
