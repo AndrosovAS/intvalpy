213cbf2ed3a86a0df2a845825554cf0211c30f30
AndrosovAS <61318188+AndrosovAS@users.noreply.github.com>
2021-11-02 17:58:32 +0700

    Обновление проекта

--- intvalpy/__init__.py
+++ intvalpy/__init__.py
@@ -1,4 +1,3 @@
-# from .RealInterval import Interval, ClassicalArithmetic, KauherArithmetic
 from .RealInterval import Interval, ClassicalArithmetic, KauherArithmetic
 from .intoper import *
 from .visualization import lineqs, IntLinIncR2, lineqs3D, IntLinIncR3, OneShotVisual2D
