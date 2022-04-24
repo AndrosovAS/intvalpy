815d28a6ae0765d214e46f8d16b998a754ca2785
AndrosovAS <artem.androsov@gmail.com>
2022-04-24 19:29:31 +0700

    hotfix. changed INSTALL_REQUIRES and increasedPrecisionQ to extendedPrecisionQ

--- intvalpy/linear/__init__.py
+++ intvalpy/linear/__init__.py
@@ -1,4 +1,4 @@
 from .overdetermined import Rohn, PSS
 from .square_system import Gauss, Gauss_Seidel, Subdiff
-from .system_properties import ive
+from .system_properties import ive, outliers
 from .recognizing_functional import Uni, Tol
--- intvalpy/linear/system_properties.py
+++ intvalpy/linear/system_properties.py
@@ -1,6 +1,7 @@
 import numpy as np
 from intvalpy.RealInterval import Interval
 from intvalpy.utils import asinterval, infinity
+from intvalpy.linear.recognizing_functional import Tol, Uni
 
 
 def ive(A, b, N=40):
@@ -40,46 +41,47 @@ def ive(A, b, N=40):
 
     return np.sqrt(A.shape[1]) * _max * cond * (np.linalg.norm(_arg_max, ord=2)/np.sqrt(sum(b.mag**2)))
 
-# def outliers(A, b, functional='uni', x0=None, tol=1e-12, maxiter=2000, method='standard deviations'):
-#
-#     def interquartile(data):
-#         q25, q75 = np.percentile(data, 25), np.percentile(data, 75)
-#         iqr = q75 - q25
-#         cut_off = iqr * 1.5
-#
-#         lower, upper = q25 - cut_off, q75 + cut_off
-#         return np.argwhere((data < lower) | (data > upper)).flatten()
-#
-#     def standard_deviations(data):
-#         # Set upper and lower limit to 3 standard deviation
-#         std, mean = np.std(data), np.mean(data)
-#         cut_off = std * 3
-#
-#         lower, upper = mean - cut_off, mean + cut_off
-#         return np.argwhere((data < lower) | (data > upper)).flatten()
-#
-#     WorkListA = asinterval(A).copy
-#     WorkListb = asinterval(b).copy
-#
-#     if functional == 'uni':
-#         _, x, _ = Uni(A, b, maxQ=True, x0=x0, tol=tol, maxiter=maxiter)
-#         tt = WorkListb.rad - (WorkListb.mid - WorkListA @ x).mig
-#
-#     elif functional == 'tol':
-#         _, _, _, tt = __tolsolvty(WorkListA.a, WorkListA.b, WorkListb.a, WorkListb.b, weight=x0, maxiter=maxiter,
-#                                   tol_f=tol, tol_x=tol, tol_g=tol)
-#     else:
-#         Exception('Данный функционал не предусмотрен.')
-#
-#     if method == 'standard deviations':
-#         outliers_index = standard_deviations(tt)
-#     elif method == 'interquartile':
-#         outliers_index = interquartile(tt)
-#     else:
-#         Exception('Данный метод не предусмотрен.')
-#
-#     index = np.delete(np.arange(WorkListA.shape[0]), outliers_index)
-#     WorkListA = WorkListA[index]
-#     WorkListb = WorkListb[index]
-#
-#     return WorkListA, WorkListb, outliers_index, tt
+def outliers(A, b, functional='uni', x0=None, tol=1e-12, maxiter=2000, method='standard deviations'):
+
+    def interquartile(data):
+        q25, q75 = np.percentile(data, 25), np.percentile(data, 75)
+        iqr = q75 - q25
+        cut_off = iqr * 1.5
+
+        lower, upper = q25 - cut_off, q75 + cut_off
+        return np.argwhere((data < lower) | (data > upper)).flatten()
+
+    def standard_deviations(data):
+        # Set upper and lower limit to 3 standard deviation
+        std, mean = np.std(data), np.mean(data)
+        cut_off = std * 3
+
+        lower, upper = mean - cut_off, mean + cut_off
+        return np.argwhere((data < lower) | (data > upper)).flatten()
+
+    WorkListA = asinterval(A).copy
+    WorkListb = asinterval(b).copy
+
+    if functional == 'uni':
+        _, x, _ = Uni(A, b, maxQ=True, x0=x0, tol=tol, maxiter=maxiter)
+        tt = WorkListb.rad - (WorkListb.mid - WorkListA @ x).mig
+
+    elif functional == 'tol':
+        _, x, _ = Tol(A, b, maxQ=True, x0=x0, tol=tol, maxiter=maxiter)
+        tt = WorkListb.rad - (WorkListb.mid - WorkListA @ x).mag
+
+    else:
+        Exception('Данный функционал не предусмотрен.')
+
+    if method == 'standard deviations':
+        outliers_index = standard_deviations(tt)
+    elif method == 'interquartile':
+        outliers_index = interquartile(tt)
+    else:
+        Exception('Данный метод не предусмотрен.')
+
+    index = np.delete(np.arange(WorkListA.shape[0]), outliers_index)
+    WorkListA = WorkListA[index]
+    WorkListb = WorkListb[index]
+
+    return WorkListA, WorkListb, outliers_index, tt
