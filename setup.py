d0e07c83f1da61f256e49290506a3bf12309de67
AndrosovAS <artem.androsov@gmail.com>
2024-06-23 23:26:02 +0700

    hotfix float type

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name='intvalpy',
-    version='1.6.3(0)',
+    version='1.6.4',
     description='IntvalPy - a Python interval computation library',
     long_description=README,
     long_description_content_type='text/markdown',
